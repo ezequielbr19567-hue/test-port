4968ea2914a575d355a7e27bb4c940d12f260b27
ezequielbr19567-hue <ezequielbr19567@gmail.com>
2026-09-17 14:36:36 -0300

    Delete luau-portfolio-base directory

--- luau-portfolio-base/app/dev/page.tsx
+++
@@ -1,5 +0,0 @@
-import AdminClient from "@/components/AdminClient";
-
-export default function DevPage() {
-  return <AdminClient />;
-}
--- luau-portfolio-base/public/projects/README.txt
+++
@@ -1,1 +0,0 @@
-Coloque aqui screenshots/GIFs dos seus projetos. Exemplos: project-1.jpg, project-2.jpg, project-3.jpg
